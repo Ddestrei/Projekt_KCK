75ec230ca2c792bf9841debb256f8161fe37b0e2
Filip Juszczak <juszczak.filip@wp.pl>
2025-05-31 20:57:20 +0200

    Dodanie metod createDealerHand, dealCard, addCard, coutAce dla klasy Dealer

--- DanteBlackjack/Blackjack.py
+++ DanteBlackjack/Blackjack.py
@@ -185,6 +185,27 @@ class Dealer:
         self.count = 0
         self.x = round(672*percents[choice])
         self.y = round(450*percents[choice])
+    def createDealerHand(self):
+        for i in range(1, 3):
+            self.addCard()
+    def dealCard(self):
+        return self.deck.getCard()
+    def addCard(self):
+        dealerCard = self.dealCard()
+        self.hand.append(dealerCard)
+        self.count += dealerCard.value
+        self.countAce()
+    def countAce(self):
+        self.low_count = sum(card.value for card in self.hand)
+        self.high_count = self.low_count
+        has_ace = any(card.label == "A" for card in self.hand)
+
+        if has_ace:
+            self.high_count += 10
+            if self.high_count > 21:
+                self.high_count = self.low_count
+
+        self.count = self.high_count if self.high_count <= 21 else self.low_count
 
 
 

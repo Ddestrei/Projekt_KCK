f0a9a0b2f13370353506d5ed1be7f424e9cd115e
Piotr <piotraugustyniak0011@gmail.com>
2025-06-08 17:30:10 +0200

    dodanie przyciskow, oraz loga

--- DanteBlackjack/Button.py
+++ DanteBlackjack/Button.py
@@ -1,33 +1,51 @@
 from Tool import *
 
+
 class Button(Tool):
     def __init__(self, x, y, button_name):
         super().__init__(x, y, button_name)
-        #super._init_.tool_image = pygame.image.load(f"DanteBlackJack/Grafika/Obiekty/{image_name}")
-        #super().__init__().tool_image = (pygame.Surface((100, 50))) #tymczasowe
-        #super().__init__().hitbox = pygame.Rect(super._init_.x,super._init_.y,super._init_.tool_image.get_width(),super._init_.tool_image.get_height())
+        # super._init_.tool_image = pygame.image.load(f"DanteBlackJack/Grafika/Obiekty/{image_name}")
+        # super().__init__().tool_image = (pygame.Surface((100, 50))) #tymczasowe
+        # super().__init__().hitbox = pygame.Rect(super._init_.x,super._init_.y,super._init_.tool_image.get_width(),super._init_.tool_image.get_height())
+
 
 x_button = 486
 y_button = 452
-x_button, y_button = scale_position(x_button,y_button,choice)
-button_log = Button(x_button, y_button,"P_Zaloguj.png")
+x_button, y_button = scale_position(x_button, y_button, choice)
+button_log = Button(x_button, y_button, "P_Zaloguj.png")
 
 x_button = 50
 y_button = 50
-x_button, y_button = scale_position(x_button,y_button,choice)
-button_0 = Button(x_button,y_button,"PP.png")
+x_button, y_button = scale_position(x_button, y_button, choice)
+button_0 = Button(x_button, y_button, "PP.png")
 
-x_button = 600
-y_button = 50
-x_button, y_button = scale_position(x_button,y_button,choice)
+x_button = 650
+y_button = 350
+x_button, y_button = scale_position(x_button, y_button, choice)
+StartGameButton = Button(x_button, y_button, "StartScreenGraphics/StartScreen_start_button.png")
+
+x_button = 650
+y_button = 440
+x_button, y_button = scale_position(x_button, y_button, choice)
+RulesButton = Button(x_button, y_button, "StartScreenGraphics/StartScreen_rules_button.png")
+
+x_button = 650
+y_button = 527
+x_button, y_button = scale_position(x_button, y_button, choice)
+SettingsButton = Button(x_button, y_button, "StartScreenGraphics/StartScreen_settings_button.png")
+
+x_button = 650
+y_button = 613
+x_button, y_button = scale_position(x_button, y_button, choice)
 ExitGameButton = Button(x_button, y_button, "StartScreenGraphics/StartScreen_exit_button.png")
 
-x_button = 50
-y_button = 400
-x_button, y_button = scale_position(x_button,y_button,choice)
-StartGameButton = Button(x_button, y_button, "StartScreenGraphics/StartScreen_start_button.png")
+x_button = 200
+y_button = 200
+x_button, y_button = scale_position(x_button, y_button, choice)
+StatisticsButton = Button(x_button, y_button, "StartScreenGraphics/StartScreen_statistics_button.png")
+
 
 x_button = 20
 y_button = 20
-x_button, y_button = scale_position(x_button,y_button,choice)
+x_button, y_button = scale_position(x_button, y_button, choice)
 lobby_to_Menu_button = Button(x_button, y_button, "lobby_to_Menu_button.png")
--- DanteBlackjack/DanteBlackjackStartScreen.py
+++ DanteBlackjack/DanteBlackjackStartScreen.py
@@ -6,7 +6,7 @@ from Screen import *
 
 class DanteBlackjackStartScreen(Screen):
     def __init__(self):
-        self.isadmin = 0
+        self.IsAdmin = True
 
     def Start(self, window, choice):
         pygame.time.Clock().tick(60)
@@ -15,11 +15,14 @@ class DanteBlackjackStartScreen(Screen):
         window.blit(background, (0, 0))
 
         logo = pygame.image.load('DanteBlackJack/Grafika/Obiekty/StartScreenGraphics/logo.png')
-        window.blit(logo, (589, 22))
+        window.blit(logo, (586, 35))
 
-        ExitGameButton.tool_draw(window)
         StartGameButton.tool_draw(window)
-        pygame.display.update()
+        RulesButton.tool_draw(window)
+        SettingsButton.tool_draw(window)
+        ExitGameButton.tool_draw(window)
 
-        if self.isadmin:
-            print("aa")
+        if self.IsAdmin:
+            StatisticsButton.tool_draw(window)
+
+        pygame.display.update()

7cfedb30cd4945ac754f4f0e61c3eac98d4afff9
Piotr <piotraugustyniak0011@gmail.com>
2025-06-08 15:47:00 +0200

    dodanie reszty grafik

--- DanteBlackjack/DanteBlackjackStartScreen.py
+++ DanteBlackjack/DanteBlackjackStartScreen.py
@@ -1,16 +1,25 @@
 import pygame
+
 pygame.init()
 from Screen import *
 
+
 class DanteBlackjackStartScreen(Screen):
+    def __init__(self):
+        self.isadmin = 0
+
     def Start(self, window, choice):
         pygame.time.Clock().tick(60)
-        window.fill((255, 0, 255)) #tymczasowe
+        background = pygame.image.load('DanteBlackJack/Grafika/Tla/StartScreen_background.png')
+        background = pygame.transform.scale(background, (resolutions[choice]))
+        window.blit(background, (0, 0))
+
+        logo = pygame.image.load('DanteBlackJack/Grafika/Obiekty/StartScreenGraphics/logo.png')
+        window.blit(logo, (589, 22))
+
         ExitGameButton.tool_draw(window)
         StartGameButton.tool_draw(window)
         pygame.display.update()
 
-
-
-
-
+        if self.isadmin:
+            print("aa")
